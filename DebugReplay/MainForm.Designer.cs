0d2a2dd4302de7a5a7cffb66e005fa918f48d924
digi <digi@skbkontur.ru>
2019-10-28 18:57:52 +0500

    Initial commit

---
+++ DebugReplay/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DebugReplay
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.durationLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(784, 661);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.durationLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 639);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // durationLabel
+            // 
+            this.durationLabel.Name = "durationLabel";
+            this.durationLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 661);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "DebugReplay";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel durationLabel;
+    }
+}
+
---
+++ DebugReplay/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DebugReplay
+{
+    public partial class MainForm : Form
+    {
+        private Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            // Рисуем изображение и передаем в элемент формы, который умеет показывать изображения
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            pictureBox.Image = BuildScene();
+            stopwatch.Stop();
+            durationLabel.Text = $"Время отрисовки: {stopwatch.ElapsedMilliseconds} миллисекунд";
+        }
+
+        private Bitmap BuildScene()
+        {
+            const int width = 600;
+            const int height = 600;
+            const int maxGeneration = 9;
+
+            var image = new Bitmap(width, height);
+            var graphics = Graphics.FromImage(image);
+
+            // Небо
+            graphics.FillRectangle(Brushes.LightSkyBlue, 0, 0, width, height / 2);
+            // Трава
+            graphics.FillRectangle(Brushes.ForestGreen, 0, height / 2, width, height);
+
+            for (var i = 0; i < 5; i++)
+            {
+                for (var j = 0; i < 3; i++)
+                {
+                    var rootX = (i * 250 - 100 * j)
+                        + 50 * random.NextDouble(); // Немного случайности в положение дерева
+                    var rootY = (height / 2 + 50) + 100 * j
+                        + 50 * random.NextDouble(); // Немного случайности в положение дерева
+                    var branchLength = 50 * (1 + 0.3 * j); // Длина ветвей зависит от перспективы: чем ближе, тем больше
+                    // Ось Y в 2D графике идет вниз, а дерево должны расти вверх, поэтому угол отрицательный.
+                    var branchAngle = -Math.PI / 2;
+                    DrawTree(graphics, rootX, rootY, branchAngle, branchLength,
+                        0, maxGeneration);
+                }
+            }
+
+            return image;
+        }
+
+        private void DrawTree(Graphics graphics,
+            double startX, double startY, double branchAngle, double branchLength,
+            int currentGeneration, int maxGeneration)
+        {
+            // Условие выхода из рекурсии
+            if (currentGeneration > maxGeneration)
+                return;
+
+            var branchPen = GetBranchPen(currentGeneration, maxGeneration);
+            double endX = startX + Math.Cos(branchAngle) * branchLength;
+            double endY = startY + Math.Sin(branchAngle) * branchLength;
+            graphics.DrawLine(branchPen, (int)startX, (int)startY, (int)endX, (int)endY);
+
+            for (var i = -1; i <= 1; i++)
+            {
+                var newBranchAngle = branchAngle + i * Math.PI / 6 * (1 + 0.4 * random.NextDouble());
+                var newBranchLength = branchLength * 0.6 * (1 + 0.2 * random.NextDouble());
+                DrawTree(graphics, endX, endY, newBranchAngle, newBranchLength,
+                    currentGeneration, maxGeneration);
+            }
+        }
+
+        private Pen GetBranchPen(int currentGeneration, int maxGeneration)
+        {
+            //// Для кэширования.
+            //// Если pen для текущего поколения уже построен и закэширован, то берем его из кэша
+            //var generationKey = BuildGenerationKey(currentGeneration, maxGeneration);
+            //if (branchPenCache.ContainsKey(generationKey))
+            //    return branchPenCache[generationKey];
+
+            // Иначе строим новый pen
+            var startBranchColor = Color.SaddleBrown;
+            var endBranchColor = Color.DarkGreen;
+
+            var red = ((maxGeneration - currentGeneration) * startBranchColor.R + currentGeneration * endBranchColor.R)
+                / maxGeneration;
+            var green = ((maxGeneration - currentGeneration) * startBranchColor.G + currentGeneration * endBranchColor.G)
+                / maxGeneration;
+            var blue = ((maxGeneration - currentGeneration) * startBranchColor.B + currentGeneration * endBranchColor.B)
+                / maxGeneration;
+            var branchColor = Color.FromArgb(red, green, blue);
+
+            var branchWidth = Math.Pow(1.5, 6 * (maxGeneration - currentGeneration) / maxGeneration);
+
+            var branchPen = new Pen(branchColor, (float)branchWidth);
+
+            //// Для кэширования.
+            //// Кэшируем построенный для поколения pen
+            //branchPenCache[generationKey] = branchPen;
+
+            return branchPen;
+        }
+
+        //// Для кэширования.
+        //private Dictionary<int[], Pen> branchPenCache = new Dictionary<int[], Pen>();
+
+        //// Для кэширования.
+        //private int[] BuildGenerationKey(int currentGeneration, int maxGeneration)
+        //{
+        //    return new[] { currentGeneration, maxGeneration };
+        //}
+    }
+}
